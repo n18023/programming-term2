cda3d1568796afd9b9a0e3f248799791a86ce5dd
n18023 <n18023@std.it-college.ac.jp>
2018-07-07 13:51:45 +0900

    7月5日の課題(訂正)

--- src/basic-c2/task20180705.py
+++ src/basic-c2/task20180705.py
@@ -1,28 +1,22 @@
 # コンビニでの買い物
 
 # 値段
-
-beal_v = 200
-otyumami_v = 100
+beer_v = 200
+otumami_v = 100
 yakitori_v = 100
 
 # 個数
-
-beal_c = 2
-otyumami_c = 1
+beer_c = 2
+otumami_c = 1
 yakitori_c = 2
 
-# 割引
-
+# 割引いた結果の率
 yakitori_w = 1 - 0.2
-point_kard = 150
 
 # 計算
-
-sum_v = ((beal_v * beal_c) + (otyumami_v * otyumami_c) + (yakitori_v * yakitori_c))
-payment = (((beal_v * beal_c) + (otyumami_v * otyumami_c) + ((yakitori_v * yakitori_w) * yakitori_c)) - point_kard)
+sum_v = ((beer_v * beer_c) + (otumami_v * otumami_c) + (yakitori_v * yakitori_c))
+payment = (((beer_v * beer_c) + (otumami_v * otumami_c) + ((yakitori_v * yakitori_w) * yakitori_c)) - 150)
 
 # 結果を表示
-
-print('買い物の合計は' , sum_v , "円")
-print('割引して貰うと' , payment , "円")
+print('買い物の合計は' , int(sum_v) , "円")
+print('割引して貰うと' , int(payment) , "円")
