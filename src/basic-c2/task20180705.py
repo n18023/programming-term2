f07237680247d444b7e79e946e125878a14d601e
n18023 <n18023@std.it-college.ac.jp>
2018-07-10 16:05:44 +0900

    7月5日の課題(訂正)

--- src/basic-c2/task20180705.py
+++ src/basic-c2/task20180705.py
@@ -11,11 +11,14 @@ otumami_c = 1
 yakitori_c = 2
 
 # 割引いた結果の率
-yakitori_w = 1 - 0.2
+yakitori_w = 0.2
+
+# 値引きするポイント
+point_card = 150
 
 # 計算
 sum_v = ((beer_v * beer_c) + (otumami_v * otumami_c) + (yakitori_v * yakitori_c))
-payment = (((beer_v * beer_c) + (otumami_v * otumami_c) + ((yakitori_v * yakitori_w) * yakitori_c)) - 150)
+payment = (((beer_v * beer_c) + (otumami_v * otumami_c) + ((yakitori_v * yakitori_w) * yakitori_c)) - point_card)
 
 # 結果を表示
 print('買い物の合計は' , int(sum_v) , "円")
