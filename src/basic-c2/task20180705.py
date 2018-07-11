a0215c6ab64ebbb8d8c6af6afdc847dd79dd5cd1
n18023 <n18023@std.it-college.ac.jp>
2018-07-11 13:45:50 +0900

    7月5日の課題(訂正)

--- src/basic-c2/task20180705.py
+++ src/basic-c2/task20180705.py
@@ -18,7 +18,7 @@ point_card = 150
 
 # 計算
 sum_v = ((beer_v * beer_c) + (otumami_v * otumami_c) + (yakitori_v * yakitori_c))
-payment = (((beer_v * beer_c) + (otumami_v * otumami_c) + ((yakitori_v * yakitori_w) * yakitori_c)) - point_card)
+payment = (((beer_v * beer_c) + (otumami_v * otumami_c) + ((yakitori_v * (1 - yakitori_w))) * (yakitori_c)) - point_card)
 
 # 結果を表示
 print('買い物の合計は' , int(sum_v) , "円")
