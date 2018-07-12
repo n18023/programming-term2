4ad3a82a75436861b7a5fd0be57babd923d38356
n18023 <n18023@std.it-college.ac.jp>
2018-07-12 12:02:10 +0900

    7月11日の課題(訂正)

--- src/basic-c2/task20180711.py
+++ src/basic-c2/task20180711.py
@@ -1,20 +1,20 @@
 # 入力値
 hand = input('何を出しますか?1:グー、2:チョキ、3:パー')
 
-# 結果のメッセージ
-def display(moko2):
-    print(moko2)
-
 # 関数
 def moko():
-    hand
+    fmt = 'CPUが{cpu_hand}を出しました。貴方の負けです。'
     if hand == str(1):
-        display('CPUがパーを出しました。貴方の負けです。')
+        s = fmt.format(cpu_hand = 'パー')
+        print(s)
     elif hand == str(2):
-            display('CPUがグーを出しました。貴方の負けです。')
+        s = fmt.format(cpu_hand = 'グー')
+        print(s)
     elif hand == str(3):
-                display('CPUがチョキを出しました。貴方の負けです。')
+        s = fmt.format(cpu_hand = 'チョキ')
+        print(s)
     else:
-        display("入力が不正です。終了します。")
+        print('入力が不正です。終了します。')
 
+# 出力結果
 moko()
