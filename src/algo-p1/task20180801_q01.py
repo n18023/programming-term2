e66dd78c3218531de5ad086dbb9030b9b6a93973
n18023 <n18023@std.it-college.ac.jp>
2018-08-01 15:48:56 +0900

    8月1日の課題

---
+++ src/algo-p1/task20180801_q01.py
@@ -0,0 +1,13 @@
+# 変数を使ってみよう
+
+# 変数greetingに文字列「こんにちは」を代入してください
+greeting = 'こんにちは'
+
+# 変数greetingの値を出力してください
+print(greeting)
+
+# 変数numberに数値の7を代入してください
+number = int(7)
+
+# 変数numberの値を出力してください
+print(number)
---
+++ src/algo-p1/task20180801_q02.py
@@ -0,0 +1,9 @@
+# 計算した結果を変数に代入してみよう
+apple_price = 200
+apple_count = 8
+
+# apple_priceとapple_countを掛けた結果を、変数total_priceに代入してください
+total_price = apple_price * apple_count
+
+# total_priceの値を出力してください
+print(total_price)
